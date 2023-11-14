8e21a4772b0bf90c751efd6fa463b85a439d7f30
GuilleFe2023 <guillerminaferreyra9@gmail.com>
2023-11-14 08:34:39 -0300

    modificando readme.md Cambios141123

--- person.py
+++ person.py
@@ -10,27 +10,28 @@ class Persona:
         self.apellido = ''
         self.dni = 0
         self.fecha_nacimiento = ''
+        self.genero= ''
         self.db = DBConn()
 
     def create(self):
         """Crear un nuevo registro"""
         query = "INSERT INTO persona VALUES (null,?,?,?,'null','null')"
-        values = (self.nombre, self.apellido, self.dni)
+        values = (self.nombre, self.apellido, self.dni, self.genero)
         self.db.ejecutar(query, values)
 
     def update(self):
         """Actualizar un registro existente"""
-        query = "UPDATE persona SET nombre = ?, apellido = ?, dni = ? WHERE idpersona = ?"
-        values = (self.nombre, self.apellido, self.dni, self.idpersona)
+        query = "UPDATE persona SET nombre = ?, apellido = ?, dni = ? WHERE idpersona = ?, genero = ?"
+        values = (self.nombre, self.apellido, self.dni, self.idpersona, self.genero)
         return self.db.ejecutar(query, values)
 
     def read_all(self):
         """Leer todos los registros"""
-        query = "SELECT idpersona, nombre , apellido , dni FROM persona"
+        query = "SELECT idpersona, nombre , apellido , genero , dni FROM persona"
         return self.db.ejecutar(query)
 
     def read(self):
-        query = "SELECT idpersona, nombre, apellido, dni FROM persona WHERE idpersona = ?"
+        query = "SELECT idpersona, nombre, apellido, genero, dni FROM persona WHERE idpersona = ?"
         values = (self.idpersona)
         return self.db.ejecutar(query, values)
     
@@ -39,3 +40,4 @@ class Persona:
         query = "DELETE FROM persona WHERE idpersona = ?"
         values = self.idpersona
         return self.db.ejecutar(query, values)
+    
